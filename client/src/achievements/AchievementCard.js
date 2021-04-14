be0d1534f3c03e5229a0cd58337a50625ecc089a
sarinazohdi <sarinazohdi123@gmail.com>
2021-04-14 14:42:44 -0600

    sarina

---
+++ client/src/achievements/AchievementCard.js
@@ -0,0 +1,241 @@
+import Axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import {Form, Button, Card, Container, ProgressBar} from 'react-bootstrap'
+import {Link, Redirect, useHistory} from 'react-router-dom'
+import logchecker from '../history/logchecker'
+import Logchecker from '../history/logchecker'
+
+export default function AchievementCard() {
+
+    const [isLoading, setLoading] = useState(true);
+    const [total, settotall] = useState([]);
+    const [today, settoday] = useState([]);
+    const [usergoal, setusergoal] = useState([]);
+
+    useEffect(()=>{
+        var d = new Date();
+
+        var month = ('0'+(d.getMonth()+1)).slice(-2);
+
+        Axios.all([
+
+            Axios.post('http://localhost:3001/api/gettotal',{
+                userid: localStorage.getItem('currentID'),
+
+            }),
+
+            Axios.post('http://localhost:3001/api/gethistory',{
+                date: localStorage.getItem('todaydate'),
+                userid: localStorage.getItem('currentID'),
+            }),
+
+            Axios.post('http://localhost:3001/api/getgoals',{
+                id: localStorage.getItem('currentID'),
+            })
+
+        ]).then(Axios.spread((response1, response2, response3)=>{
+            settotall(response1.data);
+            settoday(response2.data);
+            setusergoal(response3.data);
+            setLoading(false);
+        }));
+
+    
+        
+
+    }, []);
+
+    console.log(today);
+
+    const getmonthspent = () =>{
+
+        var percentage = (total[0][0]["SUM(DailySpend)"] / 10)*100;
+
+        
+        return percentage
+    }
+
+    const getmonthsaved = () =>{
+
+        var percentage = (total[0][0]["SUM(DailySpend)"] / 1000)*100;
+
+        return percentage
+    }
+
+    const getdailyspent = () => {
+
+        var percentage = (total[0][0]["SUM(DailySpend)"]/ 5000)*100;
+
+            return percentage
+        }
+
+
+    const getdailysave = () => {
+
+        var percentage = (total[0][0]["SUM(DailySave)"]/ 10)*100;
+
+            return percentage
+        }
+
+    const getdailysavetwenty = () => {
+
+        var percentage = (total[0][0]["SUM(DailySave)"]/ 1000)*100;
+    
+            return percentage
+        }
+    
+        const getdailysavethirty = () => {
+
+            var percentage = (total[0][0]["SUM(DailySave)"]/ 5000)*100;
+        
+                return percentage
+            }
+            
+       
+       
+        if(isLoading){
+            return(
+                <div>Loading</div>
+            )
+        }
+        else if (today[0].length == 0){
+            return(
+                <Logchecker/>
+            )
+            
+        }
+
+    return (
+        
+    <Container className = "d-flex align-items-center justify-content-center mx-auto" style={{paddingTop: "60px"}}>
+        
+        <div class="row" style={{paddingBottom:"12px"}}>
+        <div class="col-6 col-sm-4"></div>
+        <div class="col-6 col-sm-4"></div>
+        <div class="col-6 col-sm-4"></div>
+
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="spent.jpg" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Spend $10
+                    </Card.Title>
+
+                    <Form>
+                        <Form.Group id= "monthSpend">
+                            <ProgressBar now = {getmonthspent()}></ProgressBar>
+                        </Form.Group>
+                        
+                        
+
+                    </Form>
+                
+                </Card.Body>
+                
+            </Card>
+            
+            
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="spent.jpg" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Spend $1000
+                    </Card.Title>
+
+                <Form>
+                    <Form.Group id="dailySpend">
+                        <ProgressBar now = {getmonthsaved()}></ProgressBar>
+                    </Form.Group>
+                </Form>
+            </Card.Body>
+            </Card>
+           
+            
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="spent.jpg" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Spend $5000
+                    </Card.Title>
+
+                <Form>
+                <Form.Group id= "monthlySave">
+                    <ProgressBar now = {getdailyspent()}></ProgressBar>
+                </Form.Group>
+                </Form>
+            </Card.Body>
+            </Card>
+            
+        <div class="w-100" style={{padding:"6px"}}></div>
+
+        <div class="col-6 col-sm-4"></div>
+        <div class="col-6 col-sm-4"></div>
+        <div class="col-6 col-sm-4"></div>
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="save.png" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Save $10
+                    </Card.Title>
+
+                    <Form>
+                        <Form.Group id= "monthSpend">
+                            <ProgressBar now = {getdailysave()}></ProgressBar>
+                        </Form.Group>
+                        
+                        
+
+                    </Form>
+                
+                </Card.Body>
+                
+            </Card>
+       
+            
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="save.png" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Save $1000
+                    </Card.Title>
+
+                    <Form>
+                        <Form.Group id= "monthSpend">
+                            <ProgressBar now = {getdailysavetwenty()}></ProgressBar>
+                        </Form.Group>
+                        
+                        
+
+                    </Form>
+                
+                </Card.Body>
+                
+            </Card>
+            
+           
+            <Card bg = 'secondary' text = 'light' style = {{maxWidth: "250px", minWidth: "250px"}}>
+            <img src="save.png" class="card-img-top" alt=""/>
+                <Card.Body>
+                    <Card.Title>
+                    Save $5000
+                    </Card.Title>
+
+                    <Form>
+                        <Form.Group id= "monthSpend">
+                            <ProgressBar now = {getdailysavethirty()}></ProgressBar>
+                        </Form.Group>
+                        
+                        
+
+                    </Form>
+                
+                </Card.Body>
+                
+            </Card>
+        
+        </div>
+        
+    </Container>
+    )
+}
+    
--- client/src/achievements/Achievements.js
+++ client/src/achievements/Achievements.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Nav, Navbar, Form, Button} from 'react-bootstrap'
+import React, {useState, useEffect} from 'react'
+import { Nav, Navbar, Form, Button, Container, Row, Col, Card} from 'react-bootstrap'
 import {Link, Redirect, useHistory} from 'react-router-dom'
 import CurrentUser from '../contexts/CurrentUser'
 import MainNavBar from '../contexts/NavBar'
 import AdminNavBar from '../contexts/NavBarAdmin'
+import Axios from 'axios';
+import AchievementsCard from './AchievementCard';
+
 
 export default function Achievements() {
    
+    var switcher = false;
+
+    const [isLoading, setLoading] = useState(true);
+    const [uid, setUid] = useState([]);
+
     const getAdmin = () => {
         const id = localStorage.getItem('adminstate');
         console.log(id); 
@@ -20,15 +28,42 @@ export default function Achievements() {
         else{
             return <AdminNavBar/>
         }
-    }
+    };
+
+    useEffect(()=>{
+        Axios.post('http://localhost:3001/api/gethistorymain',{
+            id: localStorage.getItem('currentID'),
+        }).then((response)=>{
+            setUid(response.data);
+            console.log(uid);
+            setLoading(false);
+        });
+
+    }, []);
+
+       
+    if(isLoading){
+        return <div>Loading</div>;
+    } 
+
     return (
             
         <div>
             {renderadmin()}
 
-            
-           
+            <h1 class = 'display-1'style={{paddingLeft: "10px", paddingTop: "10px",}}>Acievements</h1>
+            <h1 class="text-l font-bold pb-6">  Wow! Great work on your Achievements so far! Here they are:</h1>
+            <Container fluid>
+            <Col>
+                <AchievementsCard/>
+            </Col>
+
+
+            </Container>
+
         </div>
+           
+
         
         
     )
